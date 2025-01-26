e4ad9c66e314bf3a0672906655be1ef19a1a2d4c
guigomes91 <guilherme.gomes91@outlook.com>
2025-01-26 10:52:01 -0300

    config de kafka events

--- src/main/java/org/br/mineradora/message/KafkaEvents.java
+++ src/main/java/org/br/mineradora/message/KafkaEvents.java
@@ -1,7 +1,22 @@
 package org.br.mineradora.message;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.br.mineradora.dto.QuotationDTO;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class KafkaEvents {
+
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvents.class);
+
+    @Channel("quotation-channel")
+    Emitter<QuotationDTO> quotationRequestEmitter;
+
+    public void sendNewKafkaEvent(QuotationDTO quotationDTO) {
+        LOG.info("-- Enviando Cotação para Tópico Kafka --");
+        quotationRequestEmitter.send(quotationDTO).toCompletableFuture().join();
+    }
 }
